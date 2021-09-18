479a6427271a4142a5b8ee146481f055b885c574
Dmitrii Butov <bdswork@mail.ru>
2021-09-18 20:29:04 +0400

    [mon] use map instead cycle to get result info

--- src/producer/site_mon.py
+++ src/producer/site_mon.py
@@ -16,24 +16,36 @@ sites = [
     'https://www.youtube.com/watch?v=YKXCRs_P-xU',
 ]
 
-#find_regex = '<h1>(.+)</h1>'
-find_regex = '<title>(.+)</title>'
+find_regex = '<h1>(.+)</h1>'
+#find_regex = '<title>(.+)</title>'
+pattern = None
+
+def init():
+    global pattern
+    pattern = re.compile(find_regex) if find_regex else None 
 
 def search(pattern, text):
-    if not pattern: 
+    if not pattern:
         return False
     result = pattern.search(text)
     return result.group(0) if result else None
 
+def action(site):
+    global pattern
+    response = requests.get(site)
+    access_time = response.elapsed.total_seconds()
+    info = '{:<70}{:<5}{:<7.3f}'.format(site, response.status_code, access_time)
+    search_result = search(pattern, response.text)
+    if search_result:
+        info += search_result
+    return info
+
 def main():
-    pattern = re.compile(find_regex) if find_regex else None 
-    for site in sites:
-        response = requests.get(site)
-        access_time = response.elapsed.total_seconds()
-        info = '{:<70}{:<5}{:<7.3f}'.format(site, response.status_code, access_time)
-        search_result = search(pattern, response.text)
-        if search_result:
-            info += search_result
+    init()
+    print("Working...")
+    info_it = map(action, sites)
+    info_list = list(info_it)    
+    for info in info_list:
         print(info)
 
 if __name__ == '__main__':
