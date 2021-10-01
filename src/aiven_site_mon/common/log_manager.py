f8f0c8c010a634175b15f69198a1ac67051c7ecb
Dmitrii Butov <bdswork@mail.ru>
2021-10-02 01:05:55 +0400

    [logger] handle exception with wrong level (from args)

--- src/aiven_site_mon/common/log_manager.py
+++ src/aiven_site_mon/common/log_manager.py
@@ -4,14 +4,20 @@ import logging
 #For developing only! - Add TRACE log level and Logger.trace() method.
 def create_trace_level(logging):
     logging.TRACE = 5
-    logging.addLevelName(logging.TRACE, "TRACE")
+    logging.addLevelName(logging.TRACE, 'TRACE')
     def _trace(logger, message, *args, **kwargs):
         if logger.isEnabledFor(logging.TRACE):
             logger._log(logging.TRACE, message, args, **kwargs)
     logging.Logger.trace = _trace
 
 def init(logger, level, show_timemark=True):
-    logger.setLevel(level)
+    try:
+        logger.setLevel(level)
+    except ValueError:
+        print('Warning: Wrong init logger level! Use INFO')
+        level='INFO'
+        logger.setLevel(level)
+
     handler = logging.StreamHandler()
     handler.setLevel(level)
     timemark = '%(asctime)s ' if show_timemark else ''
