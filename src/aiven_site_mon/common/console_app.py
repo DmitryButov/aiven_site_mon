77b739a3a9d27936bdcac5f2c525db4787562775
Dmitrii Butov <bdswork@mail.ru>
2021-10-01 11:52:13 +0400

    [app] template for app struct

--- src/aiven_site_mon/common/console_app.py
+++ src/aiven_site_mon/common/console_app.py
@@ -1,4 +1,4 @@
-import logging
+import logging, time
 from . import APP_LOGGER_NAME
 from . import log_manager
 
@@ -8,6 +8,18 @@ def main():
     log_manager.create_trace_level(logging)  #TODO use only for developing
     log_manager.init(logger, logging.TRACE)
 
-    logger.debug("debug msg")
-    logger.warning("warn msg")
-    logger.trace("trace msg")
+    logger.info("TODO Load settings")
+
+    #TODO working with cli args:
+    #- load setting json file from path from arg
+    #- select working mode (producer or consumer)
+
+    #TODO prepate to work
+    logger.info("TODO Working...")
+    try:
+        while True:
+            logger.info("Do monitoring")
+            time.sleep(1)
+    except KeyboardInterrupt:
+        #stop
+        logger.info("exit")
