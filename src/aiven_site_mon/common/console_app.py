faa0e2967dcadab5eab74ea57147f69b0ff25af2
Dmitrii Butov <bdswork@mail.ru>
2021-10-02 01:04:25 +0400

    [args] create args for select log level and operating mode

--- src/aiven_site_mon/common/console_app.py
+++ src/aiven_site_mon/common/console_app.py
@@ -6,38 +6,35 @@ import aiven_site_mon
 
 logger = logging.getLogger().getChild(APP_LOGGER_NAME)
 
-def main():
-    log_manager.create_trace_level(logging)  #TODO use only for developing
-    log_manager.init(logger, logging.TRACE)
-
-
-
-    #TODO working with cli args:
-    #- load setting json file from path from arg
-    #- select working mode (producer or consumer)
-    #- select log level (verbose level)
-
+def parse_console_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--version', action='version', version='%(prog)s ' + APP_VERSION)
-    parser.add_argument("-s", "--settings", type=pathlib.Path, default='settings.json',
-                        help="path to settings file in JSON format")
-    args = parser.parse_args()
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s ' + APP_VERSION)
+    parser.add_argument('-m', '--mode', default='console', metavar='<value>',
+                        help='operating mode selection: console, kafka-producer, kafka-consumer')
+    parser.add_argument('-s', '--settings', type=pathlib.Path, required=True, metavar='<filename.json>',
+                        help='path to settings file in JSON format')
+    parser.add_argument('-l', '--log_level', default='INFO', metavar='<level>',
+                        help='set log level: TRACE, DEBUG, INFO, WARNING, etc.')
+    return parser.parse_args()
+
+
+def main():
+    args = parse_console_args()
+    log_manager.create_trace_level(logging)  #TODO use only for developing
+    log_manager.init(logger, args.log_level)
 
     if not args.settings.is_file():
         logger.error('Settings file "{}" is not exist! Please, see usage with --help. Exit.'.format(args.settings))
         return
-
     logger.info('TODO Load settings from file: {}'.format(args.settings))
 
 
-
-
-    #TODO prepate to work
-    logger.info("TODO Working...")
+    #TODO prepare to work
+    logger.info('TODO Working...')
     try:
         while True:
-            logger.info("Do monitoring")
+            logger.info('TODO monitoring')
             time.sleep(1)
     except KeyboardInterrupt:
         #stop
-        logger.info("exit")
+        logger.info('exit')
