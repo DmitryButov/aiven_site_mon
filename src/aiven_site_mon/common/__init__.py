330718babeabfeb30e618d4e5dfd61532c3843e8
Dmitrii Butov <bdswork@mail.ru>
2021-10-01 13:16:47 +0400

    [args] add version arg, add settings file arg

--- src/aiven_site_mon/common/__init__.py
+++ src/aiven_site_mon/common/__init__.py
@@ -1,2 +1,5 @@
 
 APP_LOGGER_NAME = "site_mon"
+
+#TODO use automatic appoach, create extarnbal _version.py file and use it here and inside setup,py
+APP_VERSION = '0.1'  # note: check also setup.py!
--- src/aiven_site_mon/common/console_app.py
+++ src/aiven_site_mon/common/console_app.py
@@ -1,6 +1,8 @@
 import logging, time
-from . import APP_LOGGER_NAME
+import pathlib, argparse
+from . import APP_LOGGER_NAME, APP_VERSION
 from . import log_manager
+import aiven_site_mon
 
 logger = logging.getLogger().getChild(APP_LOGGER_NAME)
 
@@ -8,11 +10,27 @@ def main():
     log_manager.create_trace_level(logging)  #TODO use only for developing
     log_manager.init(logger, logging.TRACE)
 
-    logger.info("TODO Load settings")
+
 
     #TODO working with cli args:
     #- load setting json file from path from arg
     #- select working mode (producer or consumer)
+    #- select log level (verbose level)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--version', action='version', version='%(prog)s ' + APP_VERSION)
+    parser.add_argument("-s", "--settings", type=pathlib.Path, default='settings.json',
+                        help="path to settings file in JSON format")
+    args = parser.parse_args()
+
+    if not args.settings.is_file():
+        logger.error('Settings file "{}" is not exist! Please, see usage with --help. Exit.'.format(args.settings))
+        return
+
+    logger.info('TODO Load settings from file: {}'.format(args.settings))
+
+
+
 
     #TODO prepate to work
     logger.info("TODO Working...")
